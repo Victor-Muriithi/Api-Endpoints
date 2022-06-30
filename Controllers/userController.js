b97b0f78c46039b69c90ac3309f640bdfcb4b742
Victor-Muriithi <vmurithi10@gmail.com>
2022-06-30 15:55:43 +0300

    Error handling from Api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,10 +1,7 @@
 
 const users = require('../MOCK_DATA.json');
-
-const sqlConfig = require('../Config/dbConfig')
-const { user } = require('../Config/dbConfig')
 const poolPromise = require('../Config/pool')
-var newUsers = []
+
 
 const controllers = {
     home: ((req, res) => {
@@ -15,14 +12,22 @@ const controllers = {
     getAllusers: async (req, res) => {
         let pool = await poolPromise()
         pool.query(`SELECT * FROM userData`).then(data => {
-            newUsers = data.recordset
-            console.log(data.recordset)
-        })
-        res.json({
-            Status: 200,
-            Success: true,
-            Message: 'All users',
-            Result: newUsers
+            try {
+                res.json({
+                    Status: 200,
+                    Success: true,
+                    Message: 'All users',
+                    Result: data.recordset
+                })
+                newUsers = data.recordset
+                console.log(data.recordset)
+                return;
+
+
+            } catch (err) {
+                res.send(err)
+                console.log(err)
+            }
         })
     },
 
@@ -30,62 +35,71 @@ const controllers = {
         const { email } = req.params;
         let pool = await poolPromise()
         pool.query(`SELECT * FROM userData WHERE email = '${email}'`)
-            .then(data=>{
-                console.log(data.recordset)
-                res.send(data.recordset)
+            .then(data => {
+                if (data.recordset) {
+                    return res.status(200).json({
+                        status: 200,
+                        Success: true,
+                        Message: 'User Found',
+                        Result: data.recordset}) &&
+                        console.log(data.recordset)
+
+                }
+
+                res.status(404).json({
+                    Status: 404,
+                    Success: false,
+                    Message: 'User not found',
+                    result: []
+                })
             })
 
-        // const user = users.find((data) => data.email === email)
-        // if (user) {
-        //     return res.status(200).json({
-        //         status: 200,
-        //         Success: true,
-        //         Message: 'User Found',
-        //         Result: user
-
-        //     })}
-
-        //     res.status(404).json({
-        //         Status: 404,
-        //         Success: false,
-        //         Message: 'User not found',
-        //         result: []
-        //     })
-
-
     },
+
 
     login: async (req, res) => {
         const { email, pwd } = req.body
         let pool = await poolPromise()
         pool.query(`SELECT * FROM userData WHERE email = '${email}' AND password = '${pwd}'`)
-            // const auth = users.find((data)=>data.email===email && data.Password===pwd)
-            .then(data =>{
-                console.log(data.recordset)
-                res.send(data.recordset);
+
+            .then(data => {
+                if (data.recordset) {
+                    return res.status(200).json({
+                        status: 200,
+                        Success: true,
+                        Message: "Login Successful",
+                        result: data.recordset
+                    }) && console.log(data.recordset)
+                }
+                res.status(403).json({
+                    status: 403,
+                    Success: false,
+                    Message: "Login Failed",
+                    result: []
+                })
             })
-            //     return res.status(200).json({
-            //         status:200,
-            //         Success:true,
-            //         Message:"Login Successful",
-            //         result:auth})
-            // }
-            // res.status(403).json({
-            //     status:403,
-            //     Success:false,
-            //     Message:"Login Failed",
-            //     result:[]})
     },
+
 
     createUser: async (req, res) => {
         let { id, first_name, last_name, email, gender, pwd } = req.body
         let pool = await poolPromise()
         pool.query(`INSERT INTO userData VALUES('${id}', '${first_name}', '${last_name}', '${email}', '${gender}', '${pwd}')`)
             .then(result => {
-                if (result.rowsAffected)
-                    res.send("user added")
-                console.log("user added")
-            })
+                if (result.rowsAffected) {
+                    return res.status(200).json({
+                        status: 200,
+                        Success: true,
+                        Message: "User Added"
+                    }) && console.log("User added")
+                }
+                res.status(403).json({
+                    status:404,
+                    Success:false,
+                    Message:"User not added"
+                })
+            }
+            )
 
     },
 
@@ -95,9 +109,13 @@ const controllers = {
             users.map((user) => (
                 pool.query(`INSERT INTO userData VALUES('${user.id}', '${user.first_name}', '${user.last_name}', '${user.email}', '${user.gender}', '${user.Password}')`)
             ))
+        } try {
+            console.log('finished updating database')
+            res.send('finished updating database')
+        } catch (err) {
+            res.send(err)
+            console.log(err)
         }
-        res.send('finished updating database')
-        console.log('finished updating database')
     }
 
 
